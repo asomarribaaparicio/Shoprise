b45423e1243b19972beaf363f252ff5a5a9b2011
Ander <asomarribaa01@educantabria.es>
2020-10-26 23:50:32 +0100

    Traducción de idioma, Referencias en el string.xml y también el splas screen

--- app/src/main/java/com/example/shoprise/MainActivity.java
+++ app/src/main/java/com/example/shoprise/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.shoprise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(MainActivity.this,Login.class);
+                startActivity(splash);
+            }
+        },2000);
+        }
     }
-}
