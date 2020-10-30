16ea8cee8a357d275d2743a18d228c1c55f8d0f2
Ander <asomarribaa01@educantabria.es>
2020-10-30 15:22:22 +0100

    BOTON REDONDO, ACTIVITY PRINCIPAÑ

--- app/src/main/java/com/example/shoprise/Login.java
+++ app/src/main/java/com/example/shoprise/Login.java
@@ -2,7 +2,9 @@ package com.example.shoprise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void registro (View view)
+    {
+        Intent registrar = new Intent(this, Registro.class);
+        startActivity(registrar);
+    }
+    public void pincipalogin(View view) {
+        Intent principalogin = new Intent(this, Principal.class);
+        startActivity(principalogin);
     }
 }
--- app/src/main/java/com/example/shoprise/Registro.java
+++ app/src/main/java/com/example/shoprise/Registro.java
@@ -2,7 +2,9 @@ package com.example.shoprise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    public void pincipalregistro(View view) {
+        Intent principalregistro = new Intent(this, Principal.class);
+        startActivity(principalregistro);
     }
 }
