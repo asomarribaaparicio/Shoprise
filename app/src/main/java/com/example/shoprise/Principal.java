56f4bd048fae494a24dc40be2d296359da5cd72c
Ander <asomarribaa01@educantabria.es>
2020-10-31 18:47:23 +0100

    detalles

--- app/src/main/java/com/example/shoprise/Principal.java
+++ app/src/main/java/com/example/shoprise/Principal.java
@@ -5,19 +5,26 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        
         RecyclerView RecyclerViewM = findViewById(R.id.RecyclerView_Moda);
         RecyclerView RecyclerViewD = findViewById(R.id.ReciclerView_Deportes);
+        RecyclerView RecyclerViewT = findViewById(R.id.RecyclerView_Tecno);
         RecyclerViewM.setHasFixedSize(true);
         RecyclerViewM.setLayoutManager(new LinearLayoutManager(this));
         RecyclerViewD.setHasFixedSize(true);
         RecyclerViewD.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewT.setHasFixedSize(true);
+        RecyclerViewT.setLayoutManager(new LinearLayoutManager(this));
 
 
         Marca[] Moda = new Marca[]{
@@ -39,16 +46,31 @@ public class Principal extends AppCompatActivity {
                 new Marca(R.drawable.puma),
         };
 
+        Marca[] Tecnología = new Marca[]{
+                new Marca(R.drawable.apple),
+                new Marca(R.drawable.lg),
+                new Marca(R.drawable.xiaomi),
+                new Marca(R.drawable.samsung),
+                new Marca(R.drawable.pccomponentes),
+                new Marca(R.drawable.mediamarkt),
+        };
+
         MarcaAdapter MarcaAdapterM = new MarcaAdapter(Moda,Principal.this);
         RecyclerViewM.setAdapter(MarcaAdapterM);
-
         LinearLayoutManager layoutManagerM = new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false);
         RecyclerViewM.setLayoutManager(layoutManagerM);
 
         MarcaAdapter MarcaAdapterD = new MarcaAdapter(Deportes,Principal.this);
         RecyclerViewD.setAdapter(MarcaAdapterD);
-
         LinearLayoutManager layoutManagerD = new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false);
         RecyclerViewD.setLayoutManager(layoutManagerD);
+
+        MarcaAdapter MarcaAdapterT = new MarcaAdapter(Tecnología,Principal.this);
+        RecyclerViewT.setAdapter(MarcaAdapterT);
+        LinearLayoutManager layoutManagerT = new LinearLayoutManager(this , LinearLayoutManager.HORIZONTAL , false);
+        RecyclerViewT.setLayoutManager(layoutManagerT);
+
+
     }
-}
+
+    }
--- app/src/main/java/com/example/shoprise/Registro.java
+++ app/src/main/java/com/example/shoprise/Registro.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Registro extends AppCompatActivity {
+
+    private Spinner spinnerR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-    }
+
+        spinnerR = (Spinner) findViewById(R.id.spinner);
+
+        String [] paises =  {"España","Alemania","Portugal","Francia","Países Bajos","Grecia","Austria","Bélgica","Hungría","Irlanda","Bulgaria","Italia","Chequia","Chipre","Letonia","Lituania","Croacia","Luxemburgo","Dinamarca","Malta","Eslovaquia","Polonia","Eslovenia","Estonia","Rumania","Finlandia","Suecia"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, paises);
+        spinnerR.setAdapter(adapter);
+    };
 
     public void pincipalregistro(View view) {
         Intent principalregistro = new Intent(this, Principal.class);
         startActivity(principalregistro);
+
     }
 }
